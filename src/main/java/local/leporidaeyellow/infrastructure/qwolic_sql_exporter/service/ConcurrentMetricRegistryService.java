76ca8d425ee5b3f9c14ba96b81de855c3998f616
Eugene <158601903+leporidaeyellow@users.noreply.github.com>
2024-11-15 23:59:05 +0500

    commit v.0.2.1

--- src/main/java/local/leporidaeyellow/infrastructure/qwolic_sql_exporter/service/ConcurrentMetricRegistryService.java
+++ src/main/java/local/leporidaeyellow/infrastructure/qwolic_sql_exporter/service/ConcurrentMetricRegistryService.java
@@ -4,6 +4,9 @@ import local.leporidaeyellow.infrastructure.qwolic_sql_exporter.model.config.Met
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,18 +14,18 @@ import java.util.concurrent.ExecutionException;
 
 @Service
 public class ConcurrentMetricRegistryService {
+    private final Map<String, CompletableFuture<?>> futureMap = new ConcurrentHashMap<>();
+    private final Map<String, Instant> timestampMap = new HashMap<>();
 
     @Autowired
     SqlExporterMetricsRegistry sqlExporterMetricsRegistry;
 
-    private final Map<String, CompletableFuture<?>> map = new ConcurrentHashMap<>();
-
     public CompletableFuture<?> getFuture(String name) {
-        return map.get(name);
+        return futureMap.get(name);
     }
 
     public void setFuture(String name,CompletableFuture<?> future) {
-        map.put(name, future);
+        futureMap.put(name, future);
     }
 
     public Boolean isDoneFuture(String name) {
@@ -30,7 +33,15 @@ public class ConcurrentMetricRegistryService {
     }
 
     public Boolean isExistFuture(String name) {
-        return map.containsKey(name);
+        return futureMap.containsKey(name);
+    }
+
+    public void setTimestamp(String name, Instant timestamp) {
+        timestampMap.put(name, timestamp);
+    }
+
+    public Instant getTimestamp(String name) {
+        return timestampMap.get(name);
     }
 
     public CompletableFuture<Double> getResult(MetricEntity metric) {
@@ -41,6 +52,7 @@ public class ConcurrentMetricRegistryService {
         CompletableFuture<Double> future = getResult(metric);
         if (executePermission(metric)) {
             setFuture(metric.getConcurrentRegistryName(), future);
+            setTimestamp(metric.getConcurrentRegistryName(), Instant.now());
             sqlExporterMetricsRegistry.setValueToMetrics(metric, future);
         }
     }
@@ -49,5 +61,9 @@ public class ConcurrentMetricRegistryService {
         return getFuture(metric.getConcurrentRegistryName()) == null ||
                 isDoneFuture(metric.getConcurrentRegistryName()) ||
                 !isExistFuture(metric.getConcurrentRegistryName());
+    }
+
+    public long getDurationFromTimestampMap(MetricEntity metric) {
+        return getTimestamp(metric.getConcurrentRegistryName()).until(Instant.now(), ChronoUnit.SECONDS);
     }
 }
